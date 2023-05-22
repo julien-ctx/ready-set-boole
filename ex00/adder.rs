58c01aa48c49a6b54842d6c315cbbed74bf83f15
Julien CAUCHETEUX <jdaodkql@gmail.com>
2023-05-22 23:58:28 +0200

    refactor ex00, clarify code and change method

--- ex00/adder.rs
+++ ex00/adder.rs
@@ -1,31 +1,21 @@
-fn set_bit(nb: u32, bit_nb: u32) -> u32 {
-	return (1 << bit_nb) | nb;
+fn adder(a: u32, b: u32) -> u32 {
+    let mut result = 0;
+    let mut carry_over = 0;
+
+    for i in 0..32 {
+        let a_bit = a >> i & 1;
+        let b_bit = b >> i & 1;
+
+		// Check if only one value is 1, or all, which is necessary to set the result binary to 1.
+        let binary_pos = a_bit ^ b_bit ^ carry_over;
+		// Check if 2 or 3 values are 1, which is the condition to set carry_over to 1.
+        carry_over = (a_bit & b_bit) | (a_bit & carry_over) | (b_bit & carry_over);
+		// If binary_pos is 0, it doesn't do anything. Otherwise, it set the correct bit to 1.
+        result |= binary_pos << i;
+    }
+    result
 }
 
-fn adder(a: u32, b: u32) -> u32 {
-	let mut result: u32 = 0;
-	let mut carry_over: u32 = 0;
-	for i in 0..32 {
-		let a_bit: u32 = (a >> i) & 1;
-		let b_bit: u32 = (b >> i) & 1;
-		if carry_over == 0 {
-			
-			if a_bit & b_bit == 1 {
-				carry_over = 1;
-			} else if a_bit | b_bit != 0 {
-				result = set_bit(result, i);
-			}
-		} else {
-			if a_bit & b_bit == 1 {
-				result = set_bit(result, i);
-			} else if a_bit | b_bit == 0 {
-				result = set_bit(result, i);
-				carry_over = 0;	
-			}
-		}
-	}
-	result
-}
 
 fn main() {
 	let array: [u32; 7] = [10, 42, 56, 689, 0, 42, 2456];
