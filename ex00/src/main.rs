6d1bb851cfee62f1ea87a26cb2aaea8416f9c2fc
Julien CAUCHETEUX <jdaodkql@gmail.com>
2023-05-23 16:12:00 +0200

    before restructuring code

--- ex00/src/main.rs
+++ ex00/src/main.rs
@@ -9,11 +9,11 @@ fn adder(a: u32, b: u32) -> u32 {
         let b_bit = b >> i & 1;
 
 		// Check if only one value is 1, or all, which is necessary to set the result binary to 1.
-        let binary_pos = a_bit ^ b_bit ^ carry_over;
+        let binary = a_bit ^ b_bit ^ carry_over;
 		// Check if 2 or 3 values are 1, which is the condition to set carry_over to 1.
         carry_over = (a_bit & b_bit) | (a_bit & carry_over) | (b_bit & carry_over);
 		// If binary_pos is 0, it doesn't do anything. Otherwise, it set the correct bit to 1.
-        result |= binary_pos << i;
+        result |= binary << i;
     }
     result
 }
---
+++ ex01/src/main.rs
@@ -0,0 +1,21 @@
+fn generate_nb(value: u32) -> u32 {
+    return if value == 1 {4294967295} else {0};
+}
+
+fn multiplier(a: u32, b: u32) -> u32 {
+    let mut numbers: Vec<u32> = Vec::new();
+    for i in 0..32 {
+        let mask: u32 = generate_nb(b >> i & 1);
+        numbers.push((a & mask) << i);
+    }
+    for num in &numbers {
+        println!("{:32b} {}", num, num);
+    }
+    a
+}
+
+fn main() {
+	// println!("{}", multiplier(29, 9));
+    multiplier(29, 9);
+    0;
+}
