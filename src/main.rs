ee517b8c0a9a7f0c47d788cfbc45e99ac579c38f
Julien CAUCHETEUX <jdaodkql@gmail.com>
2023-05-23 17:31:54 +0200

    multiplier ok

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,58 @@
 mod adder;
+mod multiplier;
 
 use adder::adder;
+use multiplier::multiplier;
 use rand::Rng;
 
 fn main() {
     let bold = "\x1b[37m";
     let reset = "\x1b[0m";
     // Add tool to generate random numbers
-    // let mut rng = rand::thread_rng();
+    let mut rng = rand::thread_rng();
 
-	// println!("{}--------------------EXERCISE 00--------------------{}", bold, reset);
-    // for _ in 0..15 {
-    //     let rand1: u32 = rng.gen_range(0..=1000);
-    //     let rand2: u32 = rng.gen_range(0..=1000);
-    //     let my_result = adder(rand1, rand2);
-    //     let real_result = rand1 + rand2;
-    //     let diff = if my_result == real_result { "\x1b[32mOK\x1b[0m" } else { "\x1b[31mKO\x1b[0m" };
-    //     println!("My result: {}{}{} | Real result: {}{}{} | DIFF {}", bold, my_result, reset, bold, real_result, reset, diff);
-    // }
-    // // Tricky cases:
-    // let my_result = adder(42, 42);
-    // let real_result = 42 + 42;
-    // let diff = if my_result == real_result { "\x1b[32mOK\x1b[0m" } else { "\x1b[31mKO\x1b[0m" };
-    // println!("My result: {}{}{} | Real result: {}{}{} | DIFF {}", bold, my_result, reset, bold, real_result, reset, diff); 
+	println!("\n{}--------------------EXERCISE 00--------------------{}", bold, reset);
+    for _ in 0..15 {
+        let rand1: u32 = rng.gen_range(0..=1000);
+        let rand2: u32 = rng.gen_range(0..=1000);
+        let my_result = adder(rand1, rand2);
+        let real_result = rand1 + rand2;
+        let diff = if my_result == real_result { "\x1b[32mOK\x1b[0m" } else { "\x1b[31mKO\x1b[0m" };
+        println!("My result: {}{}{} | Real result: {}{}{} | DIFF {}", bold, my_result, reset, bold, real_result, reset, diff);
+    }
+    // Tricky cases:
+    let my_result = adder(42, 42);
+    let real_result = 42 + 42;
+    let diff = if my_result == real_result { "\x1b[32mOK\x1b[0m" } else { "\x1b[31mKO\x1b[0m" };
+    println!("My result: {}{}{} | Real result: {}{}{} | DIFF {}", bold, my_result, reset, bold, real_result, reset, diff); 
 
-    // let my_result = adder(0, 42);
-    // let real_result = 0 + 42;
-    // let diff = if my_result == real_result { "\x1b[32mOK\x1b[0m" } else { "\x1b[31mKO\x1b[0m" };
-    // println!("My result: {}{}{} | Real result: {}{}{} | DIFF {}", bold, my_result, reset, bold, real_result, reset, diff); 
+    let my_result = adder(0, 42);
+    let real_result = 0 + 42;
+    let diff = if my_result == real_result { "\x1b[32mOK\x1b[0m" } else { "\x1b[31mKO\x1b[0m" };
+    println!("My result: {}{}{} | Real result: {}{}{} | DIFF {}", bold, my_result, reset, bold, real_result, reset, diff); 
 
-	println!("{}--------------------EXERCISE 01--------------------{}", bold, reset);
-	
+	println!("\n{}--------------------EXERCISE 01--------------------{}", bold, reset);
+	// println!("{:032b}\n{:032b}", multiplier(29, 9), 29 * 9); // 261
+	// println!("{}\n{}", multiplier(29, 9), 29 * 9); // 261
+	for _ in 0..15 {
+        let rand1: u32 = rng.gen_range(0..=500);
+        let rand2: u32 = rng.gen_range(0..=500);
+        let my_result = multiplier(rand1, rand2);
+        let real_result = rand1 * rand2;
+        let diff = if my_result == real_result { "\x1b[32mOK\x1b[0m" } else { "\x1b[31mKO\x1b[0m" };
+        println!("My result: {}{}{} | Real result: {}{}{} | DIFF {}", bold, my_result, reset, bold, real_result, reset, diff);
+    }
+
+	 // Tricky cases:
+	 let my_result = multiplier(42, 42);
+	 let real_result = 42 * 42;
+	 let diff = if my_result == real_result { "\x1b[32mOK\x1b[0m" } else { "\x1b[31mKO\x1b[0m" };
+	 println!("My result: {}{}{} | Real result: {}{}{} | DIFF {}", bold, my_result, reset, bold, real_result, reset, diff); 
+ 
+	 let my_result = multiplier(0, 42);
+	 let real_result = 0 * 42;
+	 let diff = if my_result == real_result { "\x1b[32mOK\x1b[0m" } else { "\x1b[31mKO\x1b[0m" };
+	 println!("My result: {}{}{} | Real result: {}{}{} | DIFF {}", bold, my_result, reset, bold, real_result, reset, diff); 
+
     0;
 }
--- src/multiplier.rs
+++ src/multiplier.rs
@@ -1,25 +1,32 @@
-mod adder;
+pub fn adder(a: u32, b: u32) -> u32 {
+    let mut result = 0;
+    let mut carry_over = 0;
 
-use adder::adder;
+    for i in 0..32 {
+        let a_bit = a >> i & 1;
+        let b_bit = b >> i & 1;
 
-fn generate_nb(value: u32) -> u32 {
+		// Check if only one value is 1, or all, which is necessary to set the result binary to 1.
+        let binary = a_bit ^ b_bit ^ carry_over;
+		// Check if 2 or 3 values are 1, which is the condition to set carry_over to 1.
+        carry_over = (a_bit & b_bit) | (a_bit & carry_over) | (b_bit & carry_over);
+		// If binary_pos is 0, it doesn't do anything. Otherwise, it set the correct bit to 1.
+        result |= binary << i;
+    }
+    result
+}
+
+
+fn generate_mask(value: u32) -> u32 {
     return if value == 1 {4294967295} else {0};
 }
 
-fn multiplier(a: u32, b: u32) -> u32 {
-    let mut numbers: Vec<u32> = Vec::new();
+pub fn multiplier(a: u32, b: u32) -> u32 {
+    // let mut numbers: Vec<u32> = Vec::new();
+    let mut res: u32 = 0;
     for i in 0..32 {
-        let mask: u32 = generate_nb(b >> i & 1);
-        numbers.push((a & mask) << i);
+        let mask: u32 = generate_mask(b >> i & 1);
+        res = adder(res, (a & mask) << i);
     }
-    for num in &numbers {
-        println!("{:32b} {}", num, num);
-    }
-    a
-}
-
-fn main() {
-	// println!("{}", multiplier(29, 9));
-    multiplier(29, 9);
-    0;
+    res
 }
