a9826a1a6858ddfceaaf53f971f5bb54cafaef4b
Julien CAUCHETEUX <jdaodkql@gmail.com>
2023-05-31 15:01:11 +0200

    cnf: OK

--- src/conjunctive_normal_form.rs
+++ src/conjunctive_normal_form.rs
@@ -1,33 +1,55 @@
 use crate::negation_normal_form;
-// use crate::negation_normal_form::get_vars;
+
+fn get_vars(stack: &mut Vec<char>) -> (Vec<char>, Vec<char>, Vec<char>, Vec<char>) {
+    let mut copy = stack.clone();
+    let mut first = Vec::new();
+    let mut second = Vec::new();
+    let mut third = Vec::new();
+	let mut rest = Vec::new();
+
+    copy.pop();
+	let mut i = 3;
+    for c in copy.iter().rev() {
+		if i == 3 {
+			third.insert(0, *c);
+		} else if i == 2 {
+			second.insert(0, *c);
+		} else if i == 1 {
+			first.insert(0, *c);
+		} else {
+			rest.insert(0, *c);
+		}
+		if (*c).is_ascii_uppercase() {
+			i -= 1;
+		}
+	}
+	return (rest, first, second, third);
+}
 
 pub fn conjunctive_normal_form(formula: &str) -> String {
 	let mut f: Vec<char> = negation_normal_form(formula).chars().collect();
     let mut stack: Vec<char> = Vec::new();
+    let mut vars: Vec<char> = Vec::new();
 	
-    for c in &f {
-		if *c == '|' {
-			if stack.len() > 3 && stack[stack.len() - 2] == '&' {
-				let third = f[stack.len() - 1];
-				let second = stack[stack.len() - 3];
-				let first = stack[stack.len() - 4];
-				let new_chars = format!("{}{}|{}{}|&", first, second, first, third); 
-				stack.clear();
-				stack.extend(new_chars.chars());
-			} else {
-				stack.push(*c);
-			}
+	for c in &f {
+		if stack.len() > 3 && stack[stack.len() - 1] == '&' && *c == '|' {
+			let (rest, first, second, third) = get_vars(&mut stack);
+			let new_chars = format!("{}{}{}|{}{}|&", rest.iter().collect::<String>(), first.iter().collect::<String>(), second.iter().collect::<String>(), first.iter().collect::<String>(), third.iter().collect::<String>());
+			stack.clear();
+			stack.extend(new_chars.chars());
 		} else {
 			stack.push(*c);
 		}
 	}
-	
 	f = stack.clone();
 	stack.clear();
-	let mut vars: Vec<char> = Vec::new();
 
+	let mut operator: char = '|';
+	if f.iter().collect::<String>().contains("&") {
+		operator = '&';
+	}
 	for c in &f {
-		if *c == '&' || *c == '|' {
+		if *c == operator {
 			stack.push(*c);
 		} else {
 			vars.push(*c);
